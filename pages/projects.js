1ebff3fc7b327ca812468506760e64bd29b7174e
Daniel Tham <53711247+Dtham14@users.noreply.github.com>
2025-12-24 17:49:45 -0800

    project layout

--- pages/projects.js
+++ pages/projects.js
@@ -74,7 +74,7 @@ export default function Projects() {
             {/* Projects Grid */}
             <section className="pb-24">
                 <div className="max-w-6xl mx-auto px-6">
-                    <div className="grid gap-8 md:grid-cols-2 lg:grid-cols-3">
+                    <div className="grid gap-8 md:grid-cols-2 lg:grid-cols-2">
                         {projects.map((project, index) => (
                             <div
                                 key={index}
